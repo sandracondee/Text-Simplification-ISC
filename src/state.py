acfa06876bd9b5129a009991b858a400d9e80bd8
Sandra <sandracg1234@gmail.com>
2026-04-04 19:04:03 +0200

    state defined
    Co-authored-by: pfolgueira <pablofol2003@gmail.com>

---
+++ src/state.py
@@ -0,0 +1,11 @@
+from typing import TypedDict, Annotated, List
+import operator
+
+class GraphState(TypedDict):
+    complex_text: str
+    reference_text: List[str]
+    core_information: str
+    current_simplified_text: str
+    feedback_history: Annotated[List[str], operator.add]
+    iteration_count: int
+    is_approved: bool
